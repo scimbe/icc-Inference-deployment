da1c2c9458fc6e1de30a974fceda7dd665d6ac12
dev <martin.becke@haw-hamburg.de>
2025-03-14 11:58:43 +0100

    test transformer

--- terraform/main.tf
+++ terraform/main.tf
@@ -12,15 +12,29 @@ provider "docker" {
   host = "unix:///Users/dev/.colima/default/docker.sock"
 }
 
+# Variable für die Auswahl des UI-Typs
+variable "ui_type" {
+  description = "Typ der UI (openwebui oder huggingchat)"
+  type        = string
+  default     = "openwebui"
+  
+  validation {
+    condition     = contains(["openwebui", "huggingchat"], var.ui_type)
+    error_message = "Der UI-Typ muss entweder 'openwebui' oder 'huggingchat' sein."
+  }
+}
+
 # Netzwerk für die Container
 resource "docker_network" "llm_network" {
   name = "llm-network"
 }
 
-# Open WebUI Container
+# Open WebUI Container - wenn als UI-Typ ausgewählt
 resource "docker_container" "open_webui" {
+  count = var.ui_type == "openwebui" ? 1 : 0
+  
   name  = var.webui_container_name
-  image = docker_image.open_webui.image_id
+  image = docker_image.open_webui[0].image_id
   
   networks_advanced {
     name = docker_network.llm_network.name
@@ -49,7 +63,64 @@ resource "docker_container" "open_webui" {
   }
 }
 
+# HuggingChat Container - wenn als UI-Typ ausgewählt
+resource "docker_container" "huggingchat" {
+  count = var.ui_type == "huggingchat" ? 1 : 0
+  
+  name  = "${var.webui_container_name}-huggingchat"
+  image = docker_image.huggingchat[0].image_id
+  
+  networks_advanced {
+    name = docker_network.llm_network.name
+  }
+
+  # TGI-Server läuft bereits auf host.docker.internal:8000
+  env = [
+    "HF_API_URL=http://host.docker.internal:8000/v1",
+    "DEFAULT_MODEL=${var.model_name}",
+    "HF_ACCESS_TOKEN=${var.huggingface_token}",
+    "ENABLE_EXPERIMENTAL_FEATURES=true",
+    "ENABLE_THEMING=true"
+  ]
+
+  ports {
+    internal = 3000
+    external = var.webui_external_port
+  }
+
+  restart = "unless-stopped"
+
+  volumes {
+    container_path = "/data"
+    host_path      = "${var.data_volume_path}/huggingchat"
+    read_only      = false
+  }
+}
+
 # Open WebUI Image
 resource "docker_image" "open_webui" {
-  name = "ghcr.io/open-webui/open-webui:main"
+  count = var.ui_type == "openwebui" ? 1 : 0
+  name  = "ghcr.io/open-webui/open-webui:main"
+}
+
+# HuggingChat Image
+resource "docker_image" "huggingchat" {
+  count = var.ui_type == "huggingchat" ? 1 : 0
+  name  = "ghcr.io/huggingface/chat-ui:latest"
+}
+
+# Ausgabe: URL für die UI
+output "ui_url" {
+  description = "URL zur Benutzeroberfläche"
+  value       = "http://localhost:${var.webui_external_port}"
+}
+
+output "ui_type" {
+  description = "Typ der verwendeten UI"
+  value       = var.ui_type
+}
+
+output "container_name" {
+  description = "Name des Container"
+  value       = var.ui_type == "openwebui" ? (length(docker_container.open_webui) > 0 ? docker_container.open_webui[0].name : "") : (length(docker_container.huggingchat) > 0 ? docker_container.huggingchat[0].name : "")
 }
--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,19 @@
 output "webui_url" {
-  description = "URL to access Open WebUI"
+  description = "URL to access Open WebUI or HuggingChat"
   value       = "http://localhost:${var.webui_external_port}"
 }
 
+output "ui_type" {
+  description = "Type of UI deployed (openwebui or huggingchat)"
+  value       = var.ui_type
+}
 
 output "network_name" {
   description = "Docker network name"
   value       = docker_network.llm_network.name
+}
+
+output "container_name" {
+  description = "Name of the deployed container"
+  value       = var.ui_type == "openwebui" ? (length(docker_container.open_webui) > 0 ? docker_container.open_webui[0].name : "") : (length(docker_container.huggingchat) > 0 ? docker_container.huggingchat[0].name : "")
 }
